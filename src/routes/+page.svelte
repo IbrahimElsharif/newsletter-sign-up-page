505860cf653bab92c6ffc9259dd25a90eb42050b
IbrahimElsharif <Elsharif4458@outlook.com>
2023-10-20 19:41:37 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1 class="text-rose-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<main class="grid h-screen place-items-center bg-charcoalGrey">
+    <!-- Sign Up Card -->
+    <div class="bg-white flex w-1/2 h-2/3 rounded-3xl">
+        <!-- Text Side -->
+        <div class="py-16 mx-auto flex flex-col">
+            <h1 class="text-4xl font-bold">Stay updated!</h1>
+        </div>
+        <!-- Image -->
+        <div>
+            <img src=".\static\images\" alt="">
+        </div>
+    </div>
+
+</main>
+
+<style>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkSlateGrey: "hsl(234, 29%, 20%)",
+        charcoalGrey: "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)"
+      }
+    },
   },
   plugins: [],
 }
