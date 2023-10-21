2c90bcabafc540ce3d5df5de995a3bbbb0bc939b
IbrahimElsharif <Elsharif4458@outlook.com>
2023-10-21 23:39:28 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,47 @@
 </script>
 
 <main class="grid h-screen place-items-center bg-charcoalGrey">
+
     <!-- Sign Up Card -->
-    <div class="bg-white flex w-1/2 h-2/3 rounded-3xl">
+    <div class=" bg-white flex w-3/5 h-3/4 rounded-3xl">
+
         <!-- Text Side -->
-        <div class="py-16 mx-auto flex flex-col">
-            <h1 class="text-4xl font-bold">Stay updated!</h1>
+        <div class="py-20 flex flex-col space-y-6 pl-10 ">
+
+            <!-- Header -->
+            <h1 class="text-6xl  font-bold">Stay updated!</h1>
+
+            <!-- Paragraph -->
+            <p>Stay updated! Join 60,000+ product managers receiving monthly updates on:</p>
+
+            <!-- List Items -->
+            <ul class="space-y-3">
+                <li class="">
+                    <img src=".\images\icon-list.svg" class="inline mr-2" alt="">
+                    Product discovery and building what matters
+                </li>
+                <li>
+                    <img src=".\images\icon-list.svg" class="inline mr-2" alt="">
+                    Measuring to ensure updates are a success
+                </li>
+                <li>
+                    <img src=".\images\icon-list.svg" class="inline mr-2" alt="">
+                    And much more!
+                </li>
+            </ul>
+
+            <!-- Email Input -->
+            <form action="www.youtube.com" class="space-y-2">
+                <label for="email-input">Email address</label>
+                <input type="email" name="email-input" placeholder="email@company.com" id="email-input" class="block w-96 h-12 placeholder:pl-5 rounded-lg border-solid border-2">
+            </form>
+            
+            <button type="submit" class="bg-darkSlateGrey w-96 h-12 text-white rounded-lg">Subscribe to monthly newsletter</button>
         </div>
+
         <!-- Image -->
-        <div>
-            <img src=".\static\images\" alt="">
+        <div class="mx-4 my-auto">
+            <img src=".\images\illustration-sign-up-desktop.svg" alt="">
         </div>
     </div>
 
