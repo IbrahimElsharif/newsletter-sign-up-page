3cc1a06f64286e67c5d03513cea3c7f78e8d131f
IbrahimElsharif <Elsharif4458@outlook.com>
2023-10-22 23:30:02 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,24 @@
 <script>
+    import Success from "./components/Success.svelte";
+
+    //const Email = document.getElementById("email-input").nodeValue;
+
+    
 
 </script>
 
-<main class="grid h-screen place-items-center bg-charcoalGrey">
+<Success />
+
+<main class="grid h-screen place-items-center ">
 
     <!-- Sign Up Card -->
-    <div class=" bg-white flex w-3/5 h-3/4 rounded-3xl">
+    <div class=" bg-white flex w-3/5  rounded-3xl">
 
         <!-- Text Side -->
-        <div class="py-20 flex flex-col space-y-6 pl-10 ">
+        <div class="my-20 flex flex-col space-y-6 pl-10 ">
 
             <!-- Header -->
-            <h1 class="text-6xl  font-bold">Stay updated!</h1>
+            <h1 class="text-6xl font-bold">Stay updated!</h1>
 
             <!-- Paragraph -->
             <p>Stay updated! Join 60,000+ product managers receiving monthly updates on:</p>
@@ -33,12 +40,13 @@
             </ul>
 
             <!-- Email Input -->
-            <form action="www.youtube.com" class="space-y-2">
+            <form action="/" class="space-y-2">
                 <label for="email-input">Email address</label>
-                <input type="email" name="email-input" placeholder="email@company.com" id="email-input" class="block w-96 h-12 placeholder:pl-5 rounded-lg border-solid border-2">
+                <input type="email" name="email-input" placeholder="email@company.com" id="email-input" class="block w-96 h-12  pl-5 rounded-lg border-solid border-2">
             </form>
             
-            <button type="submit" class="bg-darkSlateGrey w-96 h-12 text-white rounded-lg">Subscribe to monthly newsletter</button>
+            <!-- Submit -->
+            <button type="submit" class="bg-darkSlateGrey w-96 h-12 text-white rounded-lg hover:bg-gradient-to-r from-red-500 via-red-400 to-orange-400 ">Subscribe to monthly newsletter</button>
         </div>
 
         <!-- Image -->
---
+++ src/routes/components/Success.svelte
@@ -0,0 +1,31 @@
+<script>
+
+
+</script>
+
+<main class="hidden h-screen place-items-center ">
+
+    <!-- Success Message -->
+    <div class="flex flex-col p-8 space-y-6 bg-white w-1/4 h-1/2 rounded-3xl">
+
+        <!-- Success Icon -->
+        <img src=".\images\icon-success.svg" class="w-1/6" alt="">
+
+        <!-- Subscribing Message -->
+        <h2 class="text-5xl font-bold text-darkSlateGrey">Thanks for subscribing!</h2>
+
+        <!-- Paragraph -->
+        <p class="text-sm">
+            A confirmation email has been sent to . 
+            Please open it and click the button inside to confirm your subscription.
+        </p>
+
+        <!-- Button -->
+        <button class="bg-darkSlateGrey w-90 h-12 text-white rounded-lg hover:bg-gradient-to-r from-red-500 via-red-400 to-orange-400">Dismiss message</button>
+
+    </div>
+</main>
+
+<style>
+    
+</style>
